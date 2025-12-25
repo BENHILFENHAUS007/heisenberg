83dd5d2451c87d791972bd5fcd2547588c757c29
BENHILFENHAUS007 <vjandsjchannel@gmail.com>
2025-12-25 09:33:12 +0530

    fix: Simplify index.ts to only use named exports

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,13 +1,6 @@
-// Animation Components - Clean Exports
+// Animation Components - Named Exports Only
 export { ElectricBorder } from './electric-border';
 export { StackAnimation } from './stack-animation';
 export { LightningBackground } from './lightning-background';
 export { AnimatedList, AnimatedListItem } from './animated-list';
 export { GooeyScrollbar } from './gooey-scrollbar';
-
-// Default exports
-export { default as ElectricBorderDefault } from './electric-border';
-export { default as StackAnimationDefault } from './stack-animation';
-export { default as LightningBackgroundDefault } from './lightning-background';
-export { default as AnimatedListDefault } from './animated-list';
-export { default as GooeyScrollbarDefault } from './gooey-scrollbar';
