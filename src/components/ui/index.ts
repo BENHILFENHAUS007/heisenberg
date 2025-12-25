f66de0eaf460d5ebddfe0dab8b5a346ae0436faa
BENHILFENHAUS007 <vjandsjchannel@gmail.com>
2025-12-25 09:32:51 +0530

    fix: Remove incorrect type exports from index.ts

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,13 +1,13 @@
-// Animation Components
+// Animation Components - Clean Exports
 export { ElectricBorder } from './electric-border';
 export { StackAnimation } from './stack-animation';
 export { LightningBackground } from './lightning-background';
 export { AnimatedList, AnimatedListItem } from './animated-list';
 export { GooeyScrollbar } from './gooey-scrollbar';
 
-// Re-export types if needed
-export type { default as ElectricBorderProps } from './electric-border';
-export type { default as StackAnimationProps } from './stack-animation';
-export type { default as LightningBackgroundProps } from './lightning-background';
-export type { default as AnimatedListProps } from './animated-list';
-export type { default as GooeyScrollbarProps } from './gooey-scrollbar';
+// Default exports
+export { default as ElectricBorderDefault } from './electric-border';
+export { default as StackAnimationDefault } from './stack-animation';
+export { default as LightningBackgroundDefault } from './lightning-background';
+export { default as AnimatedListDefault } from './animated-list';
+export { default as GooeyScrollbarDefault } from './gooey-scrollbar';
