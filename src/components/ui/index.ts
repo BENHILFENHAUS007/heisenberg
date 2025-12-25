652530fa5b7b72b5d20aa13ba7ac235e89d4d36b
BENHILFENHAUS007 <vjandsjchannel@gmail.com>
2025-12-25 09:26:19 +0530

    feat: Add UI components index file for clean exports

---
+++ src/components/ui/index.ts
@@ -0,0 +1,13 @@
+// Animation Components
+export { ElectricBorder } from './electric-border';
+export { StackAnimation } from './stack-animation';
+export { LightningBackground } from './lightning-background';
+export { AnimatedList, AnimatedListItem } from './animated-list';
+export { GooeyScrollbar } from './gooey-scrollbar';
+
+// Re-export types if needed
+export type { default as ElectricBorderProps } from './electric-border';
+export type { default as StackAnimationProps } from './stack-animation';
+export type { default as LightningBackgroundProps } from './lightning-background';
+export type { default as AnimatedListProps } from './animated-list';
+export type { default as GooeyScrollbarProps } from './gooey-scrollbar';
